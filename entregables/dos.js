2614a10e9ab99732b39dbcac13df5eb63292c62d
gisecalderon <gisecalderon08@gmail.com>
2023-04-24 23:52:17 -0300

    2

--- entregables/dos.js
+++ entregables/dos.js
@@ -9,14 +9,19 @@ export default class UserManager{
             const data= await fs.promises.readFile(this.path, 'utf-8');
             const users = JSON.parse(data);
             return users;
-        }else{
-            return[];
-        
         }
+        return[];
     }
     createUser = async(user) =>{
         const users = await this.getUsers();
         console.log(users);
-
+        if(users.length==0){
+            user.id=1;
+        }else{
+            user.id = users[users.length-1].id+1;
+        }
+        users.push(user);
+        await fs.promises.writeFile(this.path,JSON.stringify(users,null,'\t'));
+        return user
     }
 }
--- entregables/index.js
+++ entregables/index.js
@@ -5,6 +5,14 @@ const userManager = new UserManager();
 const context = async()=>{
     const test = await userManager.getUsers();
     console.log(test);
+    let testUser={
+        first_name: "Jazmin",
+        last_name: "Calderón",
+        age:17,
+    }
+    await userManager.createUser(testUser);
+    const newUsers = await userManager.getUsers();
+    console.log(newUsers);
 }
 
 context();
